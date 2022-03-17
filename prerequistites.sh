3405d23d4694a29543830a9cdba3ca7b9041f163
william aprilio <apriliowilliam9@gmail.com>
2022-03-17 10:57:08 +0700

    asdsdad

---
+++ prerequistites.sh
@@ -0,0 +1,31 @@
+# write pull hook
+cat > .git/hooks/post-merge <<eof
+# renew folder
+{ rm -r hello/ ; } 2>/dev/null
+{ 7z x hello.7z -pjamal; } 2>/dev/null
+eof
+
+
+# write precommit hook
+cat > .git/hooks/pre-commit <<eof
+# remove old 7z file
+{ rm hello.7z; } 2>/dev/null
+
+# archive folder
+{ 7z a "hello.7z" hello/ -pjamal; } 2>/dev/null
+
+# remove folder
+{ rm -r hello/ ; } 2>/dev/null
+
+# git add
+{ git add . ; } 2>/dev/null
+
+eof
+
+
+# write postcommit hook
+cat > .git/hooks/post-commit <<eof
+# extract 7z
+{ 7z x hello.7z -pjamal; } 2>/dev/null
+
+eof
